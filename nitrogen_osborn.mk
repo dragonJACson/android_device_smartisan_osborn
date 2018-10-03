a70c065bbdabec19cb49b11d0c5934a998af9ebd
Peter Cai <peter@typeblog.net>
2018-10-03 11:29:34 +0000

    change build fingerprint to release-keys

--- nitrogen_osborn.mk
+++ nitrogen_osborn.mk
@@ -34,9 +34,9 @@ PRODUCT_GMS_CLIENTID_BASE := android-smartisan
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="osborn" \
-    PRIVATE_BUILD_DESC="osborn-user 7.1.1 NGI77B 1 dev-keys"
+    PRIVATE_BUILD_DESC="osborn-user 7.1.1 NGI77B 1 release-keys"
 
-BUILD_FINGERPRINT := Android/osborn/osborn:7.1.1/NGI77B/1536068833:user/dev-keys
+BUILD_FINGERPRINT := Android/osborn/osborn:7.1.1/NGI77B/1536068833:user/release-keys
 
 # Sign bootable images
 PRODUCT_SUPPORTS_BOOT_SIGNER := true
