05b1cc3430561415313288b43229fbd4c5a44617
XiNGRZ <chenxingyu92@gmail.com>
2018-11-29 19:20:52 +0800

    osborn: Update blobs from 6.1.2.1-201810292004-user-ob-508d416672
    Change-Id: I48bbf288514b2daec02b0ebc370b8f91d616795b

--- skydragon_osborn.mk
+++ skydragon_osborn.mk
@@ -36,7 +36,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="osborn" \
     PRIVATE_BUILD_DESC="osborn-user 7.1.1 NGI77B 1 release-keys"
 
-BUILD_FINGERPRINT := Android/osborn/osborn:7.1.1/NGI77B/1536068833:user/release-keys
+BUILD_FINGERPRINT := Android/osborn/osborn:7.1.1/NGI77B/1540814905:user/release-keys
 
 # Sign bootable images
 PRODUCT_SUPPORTS_BOOT_SIGNER := true
